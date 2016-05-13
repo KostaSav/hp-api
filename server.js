a5e765e98654eb7effa5da8d4b02f6143c6aa646
Beth Fraser <bethanyfraser@live.co.uk>
2016-05-13 12:38:10 +0100

    server changes

--- server.js
+++ server.js
@@ -43,10 +43,8 @@ app.get('/api/characters/house/:house', function(req, res){
 
 app.use(express.static('client/build'));
 
+app.set('port', (process.env.PORT || 5000));
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
