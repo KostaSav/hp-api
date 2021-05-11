f1310ee0641e69223b95b152b65d90fe63a7298e
Gwynne S <80579057+gwynne0190@users.noreply.github.com>
2021-05-11 14:16:39 +0900

    testing adding filter for users, creators, and company-organization

--- server.js
+++ server.js
@@ -16,6 +16,51 @@ app.get('/api/characters', function(req, res){
   })
 })
 
+app.get('/api/characters/creators', function(req, res){
+  fs.readFile(CHARACTERS_JSON, function(err, data){
+    if(err) process.exit(1);
+    json = JSON.parse(data);
+    creator_array = [];
+    for(character of json){
+      if(character.creator == true){
+      creator_array.push(character);
+      }
+    }
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.json(creator_array);
+  })
+})
+
+app.get('/api/characters/companies-organizations', function(req, res){
+  fs.readFile(CHARACTERS_JSON, function(err, data){
+    if(err) process.exit(1);
+    json = JSON.parse(data);
+    companyorganization_array = [];
+    for(character of json){
+      if(character.companyOrganization == true){
+      companyorganization_array.push(character);
+      }
+    }
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.json(companyorganization_array);
+  })
+})
+
+app.get('/api/characters/users', function(req, res){
+  fs.readFile(CHARACTERS_JSON, function(err, data){
+    if(err) process.exit(1);
+    json = JSON.parse(data);
+    user_array = [];
+    for(character of json){
+      if(character.user == true){
+      user_array.push(character);
+      }
+    }
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.json(user_array);
+  })
+})
+
 app.use(express.static('public'));
 
 app.set('port', (process.env.PORT || 5000));
