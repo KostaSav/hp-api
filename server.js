a10caead89c756a2b8e5da6029b595b4a71216c7
nicogulo <nicogulobelas@gmail.com>
2021-06-18 20:13:15 +0700

    add new menu  from burger bulgel

--- server.js
+++ server.js
@@ -2,19 +2,20 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var fs = require('fs');
+const port = 3000;
 var CHARACTERS_JSON = path.join(__dirname, 'data/menu.json');
 
-app.get('/', function(req,res) {
+app.get('/', function (req, res) {
   res.sendfile('public/index.html');
 });
 
-app.get('/api/menu', function(req, res){
-  fs.readFile(CHARACTERS_JSON, function(err, data){
-    if(err) process.exit(1);
+app.get('/api/menu', function (req, res) {
+  fs.readFile(CHARACTERS_JSON, function (err, data) {
+    if (err) process.exit(1);
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.json(JSON.parse(data));
-  })
-})
+  });
+});
 
 // app.get('/api/characters/students', function(req, res){
 //   fs.readFile(CHARACTERS_JSON, function(err, data){
@@ -63,8 +64,10 @@ app.get('/api/menu', function(req, res){
 
 app.use(express.static('public'));
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', process.env.PORT || 5000);
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
 });
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
