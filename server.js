792cc129633f89c9863d8e6153a8a1018c96118e
Restyled.io <commits@restyled.io>
2022-09-17 11:58:09 +0000

    Restyled by prettier

--- server.js
+++ server.js
@@ -1,36 +1,39 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
-var characters = require('./data/characters.json')
+var characters = require("./data/characters.json");
 
-app.get('/', function(req, res) { res.sendfile('public/index.html'); });
+app.get("/", function (req, res) {
+  res.sendfile("public/index.html");
+});
 
-app.get('/api/characters', function(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+app.get("/api/characters", function (req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.json(characters);
-})
+});
 
-app.get('/api/characters/students', function(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.json(characters.filter(character => character.hogwartsStudent));
-})
+app.get("/api/characters/students", function (req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.json(characters.filter((character) => character.hogwartsStudent));
+});
 
-app.get('/api/characters/staff', function(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.json(characters.filter(character => character.hogwartsStaff));
-})
+app.get("/api/characters/staff", function (req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.json(characters.filter((character) => character.hogwartsStaff));
+});
 
-app.get('/api/characters/house/:house', function(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+app.get("/api/characters/house/:house", function (req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
   var house = req.params.house.toLowerCase();
   res.json(
-      characters.filter(character => character.house.toLowerCase() === house));
-})
+    characters.filter((character) => character.house.toLowerCase() === house)
+  );
+});
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.set('port', (process.env.PORT || 5000));
+app.set("port", process.env.PORT || 5000);
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.listen(app.get("port"), function () {
+  console.log("Node app is running on port", app.get("port"));
 });
